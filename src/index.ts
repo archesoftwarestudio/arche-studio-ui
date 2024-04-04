2fd5b069fc6e0c5e05bedd2184302a8bc86d790c
joaquin <joaquin.luna@live.com>
2024-04-04 16:03:15 -0300

    v0.0.1

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./components/actions";
+export * from "./components/data-display";
+export * from "./components/data-input";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.ts", // Asegúrate de que este archivo exporte todos tus componentes
+      name: "arche-studio-ui",
+      fileName: (format: string) => `arche-studio-ui.${format}.js`,
+    },
+    rollupOptions: {
+      // Asegúrate de externalizar las dependencias que no deban incluirse en tu paquete final
+      external: ["react", "react-dom"],
+      output: {
+        // Proporciona nombres de globals aquí para las dependencias externas
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
