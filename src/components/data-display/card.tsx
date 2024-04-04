9f891ca440ca97f8b74db53f146bcf38a717ba14
joaquin <joaquin.luna@live.com>
2024-04-04 00:37:52 -0300

    updated card: converted to fc and responsive mode added

--- src/components/data-display/card.tsx
+++ src/components/data-display/card.tsx
@@ -11,117 +11,123 @@ export interface CardProps extends React.ComponentProps<"div"> {
   body: string;
   imageUrl?: string;
   imageAlt?: string;
-  imageMode?: "standard" | "padding" | "overlay";
+  imageMode?: "default" | "padding" | "overlay";
   primaryAction?: ActionProps;
   secondaryAction?: ActionProps;
   compact?: boolean;
   neutral?: boolean;
   shadow?: boolean;
   horizontal?: boolean;
+  responsive?: boolean;
 }
 
-export class Card extends React.Component<CardProps> {
-  static defaultProps = {
+export const Card: React.FC<CardProps> = (
+  props: CardProps = {
     title: "",
     body: "",
     imageUrl: "",
     imageAlt: "",
-    imageMode: "standard",
-    primaryAction: null,
-    secondaryAction: null,
+    imageMode: "default",
     compact: false,
     neutral: false,
     shadow: true,
     horizontal: false,
-  };
-
-  private cardImageMode = {
-    standard: "",
+    responsive: false,
+  }
+) => {
+  const cardImageModes = {
+    default: "",
     padding: "",
     overlay: "image-full",
   };
 
-  private figureImageMode = {
-    standard: "",
+  const figureImageModes = {
+    default: "",
     padding: "px-10 pt-10",
     overlay: "",
   };
 
-  private imageMode = {
-    standard: "",
+  const imageModes = {
+    default: "",
     padding: "rounded-xl",
     overlay: "",
   };
 
-  private bodyImageMode = {
-    standard: "",
+  const bodyImageModes = {
+    default: "",
     padding: "items-center text-center",
     overlay: "",
   };
 
-  render() {
-    const { imageMode, horizontal, ...htmlProps } = this.props;
+  const {
+    imageMode,
+    horizontal,
+    compact,
+    neutral,
+    shadow,
+    body,
+    title,
+    imageAlt,
+    imageUrl,
+    primaryAction,
+    secondaryAction,
+    responsive,
+    ...htmlProps
+  } = props;
 
-    const cardClass = clsx(
-      "card",
-      {
-        "card-compact": this.props.compact,
-        "bg-neutral text-neutral-content": this.props.neutral,
-        "bg-base-100": !this.props.neutral,
-        "shadow-xl": this.props.shadow,
-        [`${this.cardImageMode[imageMode!]}`]: imageMode,
-        "card-side": horizontal,
-        "w-96": !horizontal,
-      },
-      this.props.className
-    );
+  const cardClass = clsx(
+    "card",
+    {
+      "card-compact": compact,
+      "bg-neutral text-neutral-content": neutral,
+      "bg-base-100": !neutral,
+      "shadow-xl": shadow,
+      [`${cardImageModes[imageMode!]}`]: imageMode,
+      "card-side": !responsive && horizontal,
+      "w-96": !responsive && !horizontal,
+      "lg:card-side": responsive,
+    },
+    htmlProps.className
+  );
 
-    const figureClass = clsx("", {
-      [`${this.figureImageMode[imageMode!]}`]: imageMode,
-    });
+  const figureClass = clsx("", {
+    [`${figureImageModes[imageMode!]}`]: imageMode,
+  });
 
-    const imageClass = clsx("", {
-      [`${this.imageMode[imageMode!]}`]: imageMode,
-    });
+  const imageClass = clsx("", {
+    [`${imageModes[imageMode!]}`]: imageMode,
+  });
 
-    const bodyClass = clsx("card-body", {
-      [`${this.bodyImageMode[imageMode!]}`]: imageMode,
-    });
+  const bodyClass = clsx("card-body", {
+    [`${bodyImageModes[imageMode!]}`]: imageMode,
+  });
 
-    return (
-      <div className={cardClass} {...htmlProps}>
-        {this.props.imageUrl && (
-          <figure className={figureClass}>
-            <img
-              src={this.props.imageUrl}
-              alt={this.props.imageAlt}
-              className={imageClass}
-            />
-          </figure>
-        )}
-        <div className={bodyClass}>
-          <h2 className="card-title">{this.props.title}</h2>
-          <p>{this.props.body}</p>
-          {this.props.primaryAction && (
-            <div className="card-actions justify-end">
+  return (
+    <div className={cardClass} {...htmlProps}>
+      {imageUrl && (
+        <figure className={figureClass}>
+          <img src={imageUrl} alt={imageAlt} className={imageClass} />
+        </figure>
+      )}
+      <div className={bodyClass}>
+        <h2 className="card-title">{title}</h2>
+        <p>{body}</p>
+        {primaryAction && (
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={primaryAction.onClick}>
+              {primaryAction.text}
+            </button>
+            {secondaryAction && (
               <button
-                className="btn btn-primary"
-                onClick={this.props.primaryAction.onClick}
+                className="btn btn-secondary"
+                onClick={primaryAction.onClick}
               >
-                {this.props.primaryAction.text}
+                {primaryAction.text}
               </button>
-              {this.props.secondaryAction && (
-                <button
-                  className="btn btn-secondary"
-                  onClick={this.props.primaryAction.onClick}
-                >
-                  {this.props.primaryAction.text}
-                </button>
-              )}
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
