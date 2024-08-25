8608654f2db926739a0cbd893fd19ed2f18a039c
Joaquin <joaquin.luna@live.com>
2024-08-25 18:43:02 -0300

    exported new sections

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./components/actions";
 export * from "./components/data-display";
 export * from "./components/data-input";
+export * from "./components/feedback";
+export * from "./components/navigation";
