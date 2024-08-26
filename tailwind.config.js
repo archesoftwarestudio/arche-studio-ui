e702ec41e40344c3347c15c6f5d95a8a163c4614
Joaquin <joaquin.luna@live.com>
2024-08-26 19:27:26 -0300

    css prefix added

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ export default {
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    prefix: "arche-studio", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ":root", // The element that receives theme color CSS variables
   },
