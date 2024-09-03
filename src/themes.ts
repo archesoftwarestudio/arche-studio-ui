8cf66d93a8a268eda4a7af857cd5c1ac1bdd8f19
Joaquin <joaquin.luna@live.com>
2024-09-03 14:45:23 -0300

    update color palette

--- src/themes.ts
+++ src/themes.ts
@@ -1,13 +1,24 @@
 export interface ThemeProps {
   primary: string;
+  "primary-content": string;
   secondary: string;
+  "secondary-content": string;
   accent: string;
+  "accent-content": string;
   neutral: string;
+  "neutral-content": string;
   "base-100": string;
+  "base-200"?: string;
+  "base-300"?: string;
+  "base-content"?: string;
   info: string;
+  "info-content"?: string;
   success: string;
+  "success-content"?: string;
   warning: string;
+  "warning-content"?: string;
   error: string;
+  "error-content"?: string;
   "--rounded-box": string;
   "--rounded-btn": string;
   "--rounded-badge": string;
@@ -25,23 +36,34 @@ export interface ThemesProps {
 
 export const Themes: ThemesProps = {
   light: {
-    primary: "#02875f",
-    secondary: "#036046",
-    accent: "rgb(255, 225, 217)",
-    neutral: "#63d7b1",
-    "base-100": "#fcfcfa",
-    info: "#00b5ff",
-    success: "#00a96e",
-    warning: "#ffbe00",
-    error: "#ff5861",
-    "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
-    "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
-    "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
-    "--animation-btn": "0.25s", // duration of animation when you click on button
-    "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
-    "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
-    "--border-btn": "1px", // border width of buttons
-    "--tab-border": "1px", // border width of tabs
-    "--tab-radius": "0.5rem", // border radius of tabs
+    primary: "#002129", // gunmetal
+    "primary-content": "#ffffff", // contenido legible para primary
+    secondary: "#3fa385", // zomp
+    "secondary-content": "#ffffff", // dimgray
+    accent: "#fff0e6", // soft-crayola
+    "accent-content": "#ef7b45", // crayola o contenido legible
+    neutral: "#364853", // charcoal
+    "neutral-content": "#b1b3bb", // frenchgray
+    "base-100": "#f6f7f8", // seasalt
+    "base-200": "#d4d5da", // platinum
+    "base-300": "#e5e6e9", // platinumshiny
+    "base-content": "#002129", // gunmetal
+    info: "#b1b3bb", // frenchgray
+    "info-content": "#ffffff", // contenido legible
+    success: "#26570b", // color-success
+    "success-content": "#ffffff", // contenido legible
+    warning: "#f2c535", // color-warning
+    "warning-content": "#000000", // contenido legible
+    error: "#b11705", // color-danger
+    "error-content": "#ffffff",
+    "--rounded-box": "1rem",
+    "--rounded-btn": "0.5rem",
+    "--rounded-badge": "1.9rem",
+    "--animation-btn": "0.25s",
+    "--animation-input": "0.2s",
+    "--btn-focus-scale": "0.95",
+    "--border-btn": "1px",
+    "--tab-border": "1px",
+    "--tab-radius": "0.5rem",
   },
 };
